a1434e5d82fffac76449ce9d7c2f2625e07ec65e
milabadoo <ludmila.badu@gmail.com>
2018-12-05 21:34:36 +0300

    new tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -7,14 +7,28 @@ namespace Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("1")]
+        [InlineData("2")]
+        public void Test1(string str)
         {
             ValuesController vs = new ValuesController();
-            var actionResult = vs.Get("2");
+            var actionResult = vs.Get(str);
             String result = actionResult.Value;
-            Assert.Equal("2", result);
+            Assert.Equal(str, result);
            
+        }
+
+        [Theory]
+        [InlineData(1,2)]
+        [InlineData(10,555)]
+        public void Test2(int a, int b)
+        {
+            ValuesController vs = new ValuesController();
+            var actionResult = vs.Get(a,b);
+            var result = actionResult.Result;
+            Assert.Equal("Hello world!", result.ToString());
+
         }
     }
 }
